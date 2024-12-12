48e3516aaef07f8ad437aa9c4e492fc8d7dfb259
andrelele <nrgnevskyfrontak@gmail.com>
2024-12-12 14:58:40 +0300

    fix command

--- src/Enum/Commands.php
+++ src/Enum/Commands.php
@@ -16,7 +16,7 @@ final class Commands
     public const POST_INSTALL_CMD = ['vendor/bin/cghooks add --git-dir=.git'];
     public const POST_UPDATE_CMD = ['vendor/bin/cghooks update --git-dir=.git'];
     public const CODE_STYLE_PHPLINT = 'php -l -f';
-    public const CODE_STYLE_FIX = 'docker run -it --rm -v $(pwd):/code ghcr.io/php-cs-fixer/php-cs-fixer:3-php8.3 fix --path-mode=intersection --config .php_cs-fixer.php --allow-risky=yes';
-    public const CODE_STYLE_CHECK = 'docker run -it --rm -v $(pwd):/code ghcr.io/php-cs-fixer/php-cs-fixer:3-php8.3 fix --path-mode=intersection --config .php_cs-fixer.php --allow-risky=yes --dry-run';
+    public const CODE_STYLE_FIX = 'docker run -it --rm -v $(pwd):/code ghcr.io/php-cs-fixer/php-cs-fixer:3-php8.3 fix --path-mode=intersection --config ./vendor/futsalshuffle/cs-laravel/.php_cs-fixer.php --allow-risky=yes';
+    public const CODE_STYLE_CHECK = 'docker run -it --rm -v $(pwd):/code ghcr.io/php-cs-fixer/php-cs-fixer:3-php8.3 fix --path-mode=intersection --config ./vendor/futsalshuffle/cs-laravel/.php_cs-fixer.php --allow-risky=yes --dry-run';
     public const CODE_STYLE_ANALYZE = 'docker run --rm -v .:/app ghcr.io/phpstan/phpstan:2-php8.3 analyse -c phpstan.neon --ansi';
 }
